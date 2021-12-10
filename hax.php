743b12495989b6af1685885be829c551aa55da9d
YellowFace <68597070+yellowface233@users.noreply.github.com>
2021-12-10 20:56:51 +0800

    Update: 支持检测EU-MID1区是否可开通

--- hax.php
+++ hax.php
@@ -1,17 +1,28 @@
 <?php
 header("Content-type: application/json");
-$version = "1.2"; // 每次commit请将此数字增加0.1，用于显示版本号
+$version = "1.3"; // 每次commit请将此数字增加0.1，用于显示版本号
+
+// 获取server页面（eu1-5）
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://hax.co.id/server");
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/server)');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$head = curl_exec($ch);
+$server = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
+// 获取开通页面（eu-mid1）
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://hax.co.id/create-vps");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/server)');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$create = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
 
-file_put_contents("./data.txt",$head);
+file_put_contents("./data.txt",$server);
 
 function getLine($file, $line, $length = 40960){
     $returnTxt = null;
@@ -58,7 +69,13 @@ $e1 = preg_replace('<<h1 class="card-text">>', '', $e);
 $e2 = preg_replace('<</h1>>', '', $e1);
 $e3 = preg_replace('< VPS>', '', $e2);
 $e4 = preg_replace('/\n/', '',$e3 );
-
-echo '{"eu1":"'.$a4.'","eu2":"'.$b4.'","eu3":"'.$c4.'","eu4":"'.$d4.'","eu5":"'.$e4.'","version":'.$version.'}';
+//eu-mid-1
+$midtext = '<option value="EU Middle Specs">EU Middle Specs (KVM + SSD)</option>';
+if(substr($create,$midtext)){
+    $mid1 = "true";
+}else{
+    $mid1="false";
+}
+echo '{"eu1":"'.$a4.'","eu2":"'.$b4.'","eu3":"'.$c4.'","eu4":"'.$d4.'","eu5":"'.$e4.'","eu-mid-1":'.$mid1.',"version":"'.$version.'"}';
 unlink("./data.txt");
 ?>
