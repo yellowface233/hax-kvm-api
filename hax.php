8b63bd73856893eb812ff092c8f73e308106766c
YellowFace <68597070+yellowface233@users.noreply.github.com>
2021-12-15 19:38:42 +0800

    ♥大改: 修复行数获取错误和增加eu5~eu10、更换检测链接

--- hax.php
+++ hax.php
@@ -1,12 +1,12 @@
 <?php
 header("Content-type: application/json");
-$version = "1.3"; // 每次commit请将此数字增加0.1，用于显示版本号
+$version = "1.5"; // 每次commit请将此数字增加[大改0.2小改0.1]，用于显示版本号
 
-// 获取server页面（eu1-5）
+// 获取server页面（eu1-10）
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://hax.co.id/server");
+curl_setopt($ch, CURLOPT_URL, "https://hax.co.id/data-center");
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/server)');
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/data-center)');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $server = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -16,7 +16,7 @@ curl_close($ch);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://hax.co.id/create-vps");
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/server)');
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;https://hax.co.id/create-vps)');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $create = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -39,11 +39,20 @@ function getLine($file, $line, $length = 40960){
     }
     return $returnTxt;
 }
-$a = getLine("data.txt",100,40960);
-$b = getLine("data.txt",108,40960);
-$c = getLine("data.txt",116,40960);
-$d = getLine("data.txt",124,40960);
-$e = getLine("data.txt",132,40960);
+$a = getLine("data.txt",104,40960);
+$b = getLine("data.txt",112,40960);
+$c = getLine("data.txt",120,40960);
+$d = getLine("data.txt",128,40960);
+$e = getLine("data.txt",136,40960);
+
+// 妈的hax站长可真勤劳 一下子加5个DC
+// 直接在源码吐槽了
+$f = getLine("data.txt",148,40960);
+$g = getLine("data.txt",156,40960);
+$h = getLine("data.txt",164,40960);
+$i = getLine("data.txt",172,40960);
+$j = getLine("data.txt",180,40960);
+
 //eu1
 $a1 = preg_replace('<<h1 class="card-text">>', '', $a);
 $a2 = preg_replace('<</h1>>', '', $a1);
@@ -69,6 +78,31 @@ $e1 = preg_replace('<<h1 class="card-text">>', '', $e);
 $e2 = preg_replace('<</h1>>', '', $e1);
 $e3 = preg_replace('< VPS>', '', $e2);
 $e4 = preg_replace('/\n/', '',$e3 );
+//eu6
+$f1 = preg_replace('<<h1 class="card-text">>', '', $f);
+$f2 = preg_replace('<</h1>>', '', $f1);
+$f3 = preg_replace('< VPS>', '', $f2);
+$f4 = preg_replace('/\n/', '',$f3 );
+//eu7
+$g1 = preg_replace('<<h1 class="card-text">>', '', $g);
+$g2 = preg_replace('<</h1>>', '', $g1);
+$g3 = preg_replace('< VPS>', '', $g2);
+$g4 = preg_replace('/\n/', '',$g3 );
+//eu8
+$h1 = preg_replace('<<h1 class="card-text">>', '', $h);
+$h2 = preg_replace('<</h1>>', '', $h1);
+$h3 = preg_replace('< VPS>', '', $h2);
+$h4 = preg_replace('/\n/', '',$h3 );
+//eu9
+$i1 = preg_replace('<<h1 class="card-text">>', '', $i);
+$i2 = preg_replace('<</h1>>', '', $i1);
+$i3 = preg_replace('< VPS>', '', $i2);
+$i4 = preg_replace('/\n/', '',$i3 );
+//eu10
+$j1 = preg_replace('<<h1 class="card-text">>', '', $j);
+$j2 = preg_replace('<</h1>>', '', $j1);
+$j3 = preg_replace('< VPS>', '', $j2);
+$j4 = preg_replace('/\n/', '',$j3 );
 //eu-mid-1
 $midtext = '<option value="EU Middle Specs">EU Middle Specs (KVM + SSD)</option>';
 if(strpos($create,$midtext)){
@@ -76,6 +110,6 @@ if(strpos($create,$midtext)){
 }else{
     $mid1="false";
 }
-echo '{"eu1":"'.$a4.'","eu2":"'.$b4.'","eu3":"'.$c4.'","eu4":"'.$d4.'","eu5":"'.$e4.'","eu-mid-1":'.$mid1.',"version":"'.$version.'"}';
+echo '{"eu1":"'.$a4.'","eu2":"'.$b4.'","eu3":"'.$c4.'","eu4":"'.$d4.'","eu5":"'.$e4.'","eu6":"'.$f4.'","eu7":"'.$g4.'","eu8":"'.$h4.'","eu9":"'.$i4.'","eu10":"'.$j4.'","eu-mid-1":'.$mid1.',"version":"'.$version.'"}';
 unlink("./data.txt");
 ?>
